af3362ab78feee95c3df8017d1f60f1864a66145
Steven A. Zahm <shazahm1@hotmail.com>
2019-09-05 12:58:19 -0400

    Convert plugin file to be singleton.

--- easy-plugins-display-posts.php
+++ easy-plugins-display-posts.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Easy_Plugins\Display_Posts;
+namespace Easy_Plugins;
 
 /**
  * @package   Easy Plugins: Display Posts
@@ -21,41 +21,154 @@ namespace Easy_Plugins\Display_Posts;
  * Domain Path:       /languages
  */
 
-spl_autoload_register(
-	function( $class ) {
+final class Display_Posts {
 
-		$registry = array(
-			'Easy_Plugins\Display_Posts\Query'                   => 'includes/class.query.php',
-			'Easy_Plugins\Display_Posts\Shortcode\Display_Posts' => 'includes/class.shortcode-display-posts.php',
-			'Easy_Plugins\Display_Posts\Cache'                   => 'includes/class.cache.php',
-			'Easy_Plugins\Display_Posts\Fragment'                => 'includes/class.cache.php',
+	const VERSION = '1.0';
+
+	/**
+	 * @var Display_Posts Stores the instance of this class.
+	 *
+	 * @since 1.0
+	 */
+	private static $instance;
+
+	/**
+	 * @var string The absolute path this this file.
+	 *
+	 * @since 1.0
+	 */
+	private $file = '';
+
+	/**
+	 * @var string The URL to the plugin's folder.
+	 *
+	 * @since 1.0
+	 */
+	private $url = '';
+
+	/**
+	 * @var string The absolute path to this plugin's folder.
+	 *
+	 * @since 1.0
+	 */
+	private $path = '';
+
+	/**
+	 * @var string The basename of the plugin.
+	 *
+	 * @since 1.0
+	 */
+	private $basename = '';
+
+	/**
+	 * A dummy constructor to prevent the class from being loaded more than once.
+	 *
+	 * @since 1.0
+	 */
+	protected function __construct() { /* Do nothing here */ }
+
+	/**
+	 * The main plugin instance.
+	 *
+	 * @since 1.0
+	 *
+	 * @return self
+	 */
+	public static function instance() {
+
+		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof self ) ) {
+
+			self::$instance = $self = new self;
+
+			$self->file     = __FILE__;
+			$self->url      = plugin_dir_url( $self->file );
+			$self->path     = plugin_dir_path( $self->file );
+			$self->basename = plugin_basename( $self->file );
+
+			$self->includeDependencies();
+			$self->hooks();
+		}
+
+		return self::$instance;
+
+	}
+
+	/**
+	 * @since 1.0
+	 */
+	private function hooks() {
+
+		add_shortcode( 'display-posts', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'cache' ) );
+		//add_action( 'save_post', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
+		//add_action( 'created_term', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
+		//add_action( 'edit_term', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
+		//add_action( 'activated_plugin', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
+		//add_action( 'deactivated_plugin', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
+	}
+
+	/**
+	 * @since 1.0
+	 */
+	private function includeDependencies() {
+
+		spl_autoload_register(
+			function( $class ) {
+
+				$registry = array(
+					'Easy_Plugins\Display_Posts\Query'                   => 'includes/class.query.php',
+					'Easy_Plugins\Display_Posts\Shortcode\Display_Posts' => 'includes/class.shortcode-display-posts.php',
+					'Easy_Plugins\Display_Posts\Cache'                   => 'includes/class.cache.php',
+					'Easy_Plugins\Display_Posts\Fragment'                => 'includes/class.cache.php',
+				);
+
+				if ( ! isset( $registry[ $class ] ) ) {
+
+					return;
+				}
+
+				$file = plugin_dir_path( __FILE__ ) . $registry[ $class ];
+
+				// if the file exists, require it
+				if ( file_exists( $file ) ) {
+
+					require $file;
+
+				} else {
+
+					wp_die( esc_html( "The file attempting to be loaded at $file does not exist." ) );
+				}
+			}
 		);
 
-		if ( ! isset( $registry[ $class ] ) ) {
-
-			return;
-		}
-
-		$file = plugin_dir_path( __FILE__ ) . $registry[ $class ];
-
-		// if the file exists, require it
-		if ( file_exists( $file ) ) {
-
-			require $file;
-
-		} else {
-
-			wp_die( esc_html( "The file attempting to be loaded at $file does not exist." ) );
-		}
+		require_once 'includes/inc.format.php';
+		require_once 'includes/inc.functions.php';
 	}
-);
 
-require_once 'includes/inc.format.php';
-require_once 'includes/inc.functions.php';
+	/**
+	 * @since 1.0
+	 */
+	public function getPath() {
 
-add_shortcode( 'display-posts', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'cache' ) );
-//add_action( 'save_post', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
-//add_action( 'created_term', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
-//add_action( 'edit_term', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
-//add_action( 'activated_plugin', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
-//add_action( 'deactivated_plugin', array( 'Easy_Plugins\Display_Posts\Shortcode\Display_Posts', 'clear_cache' ) );
+		return $this->path;
+	}
+
+	/**
+	 * @since 1.0
+	 */
+	public function getURL() {
+
+		return $this->url;
+	}
+}
+
+/**
+ * @since 1.0
+ *
+ * @return Display_Posts
+ */
+function Display_Posts() {
+
+	return Display_Posts::instance();
+}
+
+Display_Posts();
